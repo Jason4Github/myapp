2911dcdbb220d82acec5664e90d8c98926d79a33
jason_yu <yuhaishengyhs@gmail.com>
2015-09-21 08:01:37 +0000

    add one: check

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,13 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+    def setup
+        @user = User.new(name: "Jason", email: "Jason@dazzle.com")
+    end
+
+    test "should be valid" do
+        assert @user.valid?
+    end
+
+ 
 end
