435116c6a125a050ade9a0b748b0707ce0e3b6ee
jason_yu <yuhaishengyhs@gmail.com>
2015-10-07 09:01:48 +0000

    add password to schema

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,12 +5,16 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
     def setup
-        @user = User.new(name: "Jason", email: "Jason@dazzle.com")
+        @user = User.new(name: "Jason_test2", email: "Jason@dazzle.com", 
+                         password: "Jason", password_confirmation: "Jason")
     end
 
     test "should be valid" do
         assert @user.valid?
     end
 
- 
+    test "password should have a minimum length" do
+        @user.password = @user.password_confirmation = "a" * 5
+        assert_not @user.valid?
+    end 
 end
